64d008f647cb6db0c6c5000b6236c8e048900291
123rishujha <jharishu796@gmail.com>
2025-04-27 10:13:27 +0530

    bug fixed

--- client/src/components/user/book/BookForm.jsx
+++ client/src/components/user/book/BookForm.jsx
@@ -83,7 +83,8 @@ function BookForm() {
   const handleBookSelect = (book) => {
     const volumeInfo = book.volumeInfo;
     const imageLink = volumeInfo.imageLinks?.thumbnail || "";
-
+  
+    // Set form values first
     formik.setValues({
       ...formik.values,
       title: volumeInfo.title || "",
@@ -92,9 +93,12 @@ function BookForm() {
       coverImage: imageLink,
       googleBookId: book.id,
     });
-
+  
     setPreviewLink(imageLink);
     setImportMode(false);
+    
+    // Submit the form after setting values
+    formik.submitForm();
   };
 
   const handleCancel = (e) => {
